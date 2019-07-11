43c6025ed434ec440249355c5bb74991a41ad4e3
Michael Lekrans <mlekrans@hotmail.com>
2019-07-11 08:45:06 +0200

    Initial Commit

---
+++ IOS_progress_bar_and_slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_progress_bar_and_slider
+//
+//  Created by Michael Lekrans on 2019-07-11.
+//  Copyright © 2019 Michael Lekrans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
